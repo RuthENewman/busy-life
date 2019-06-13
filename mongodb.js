29d29a82b8577685f2cd631935c996b182a6a0d4
Ruth Newman <ruthnewman@gmail.com>
2019-06-13 16:32:49 +0100

    exploring object ids

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,17 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+const ObjectID = mongodb.ObjectID;
+
+// const { MongoClient, ObjectID } = require('mongdodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'busy-life';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp())
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) => {
   if (error) {
@@ -11,36 +20,47 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) =
 
   const db = client.db(databaseName)
 
-  db.collection('tasks').insertMany([
-    {
-      description: 'Eat at Borough Market',
-      completed: false,
-    },
-    {
-      description: 'Take photos at Houses of Parliament',
-      completed: false,
-    },
-    {
-      description: 'Visit Van Gogh exhibition',
-      completed: true,
-    },
-    {
-      description: 'Take in the view at Sky Garden',
-      completed: false,
-    },
-    {
-      description: 'Walk by the river to Tower Bridge',
-      completed: false,
-    },
-    {
-      description: 'Watch a show on the West End',
-      completed: false,
-    }
-  ], (error, result) => {
-    if (error) {
-      return console.log('Unable to insert tasks')
-    }
-
-    console.log(result.ops)
-  })
+  // db.collection('users').insertOne({
+  //   _id: id,
+  //   name: 'Ruth',
+  //   age: 30
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user')
+  //   }
+  //   console.log(result.ops)
+  // })
+  //
+  // db.collection('tasks').insertMany([
+  //   {
+  //     description: 'Eat at Borough Market',
+  //     completed: false,
+  //   },
+  //   {
+  //     description: 'Take photos at Houses of Parliament',
+  //     completed: false,
+  //   },
+  //   {
+  //     description: 'Visit Van Gogh exhibition',
+  //     completed: true,
+  //   },
+  //   {
+  //     description: 'Take in the view at Sky Garden',
+  //     completed: false,
+  //   },
+  //   {
+  //     description: 'Walk by the river to Tower Bridge',
+  //     completed: false,
+  //   },
+  //   {
+  //     description: 'Watch a show on the West End',
+  //     completed: false,
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert tasks')
+  //   }
+  //
+  //   console.log(result.ops)
+  // })
 })
