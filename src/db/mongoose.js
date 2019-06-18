4320424faa6b41d217b5e8173c0482c99add0b35
Ruth Newman <ruthnewman@gmail.com>
2019-06-18 14:42:14 +0100

    with data validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/busy-life-api', {
   useNewUrlParser: true,
@@ -7,23 +8,57 @@ mongoose.connect('mongodb://127.0.0.1:27017/busy-life-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email address is invalid')
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
   }
 })
 
-// const me = new User({
-//   name: 'Ruth',
-//   age: 30
-// })
-//
-// me.save().then(() => {
-//   console.log(me)
-// }).catch((error) => {
-//   console.log('Oops there\'s an error: ', error)
-// })
+const me = new User({
+  name: 'Ruth',
+  age: 30,
+  email: 'ruth@gmail.com'
+})
+
+
+me.save().then(() => {
+  console.log(me)
+}).catch((error) => {
+  console.log('Oops there\'s an error: ', error)
+})
+
+const someoneElse = new User({
+  name: 'Joe',
+  email: 'joe@gmail.com'
+})
+
+someoneElse.save().then(() => {
+  console.log(someoneElse)
+}).catch((error) => {
+  console.log('Oops there\'s an error: ', error)
+})
+
+
 
 const Task = mongoose.model('Task', {
   description: {
