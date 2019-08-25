558503b8eb60831642e56bb9153b4196acd318bd
Ruth Newman <ruthnewman@gmail.com>
2019-08-25 23:57:17 +0100

    refactored out task as separate file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.post("/users", (request, response) => {
-  response.send("testing");
+  const user = new User(request.body);
+
+  user
+    .save()
+    .then(() => {
+      response.send(user);
+    })
+    .catch(error => {
+      response.status(400);
+      response.send(error);
+    });
+});
+
+app.post("/tasks", (request, response) => {
+  const task = new Task(request.body);
+
+  task
+    .save()
+    .then(() => {
+      response.send(task);
+    })
+    .catch(error => {
+      response.status(400).send(error);
+    });
 });
 
 app.listen(port, () => {
